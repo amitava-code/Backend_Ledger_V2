4f895ba3b74710f8cc134ffae4726a6d72724233
Delta Student <217562233+amitava-code@users.noreply.github.com>
2026-06-25 14:25:25 +0530

    chore(init): setup server, express app, DB connection, and DNS config

---
+++ server.js
@@ -0,0 +1,12 @@
+require("dotenv").config()
+
+require("./src/config/dns")
+const app = require("./src/app")
+const connectToDB = require("./src/config/db")
+
+connectToDB()
+
+
+app.listen(3000, ()=>{
+    console.log("Server is running on port 3000")
+})
